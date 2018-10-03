2760aee3d3fa400ff67ccbf9f22db3a7ae1ed245
Patrick Douglas <pmdouglas@gmail.com>
2018-10-03 17:16:16 -0400

    clean up
    cleaning up code and making some basic libraries

---
+++ app/databaseMethods.js
@@ -0,0 +1,105 @@
+var db = null;
+var	dbDetails = new Object();
+var rawTransactionsCollection;
+var transactionsCollection; 
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1:27017/chestshop';
+var	mongoURLLabel = "";
+
+if (mongoURL == null) {
+	var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+	// If using plane old env vars via service discovery
+	if (process.env.DATABASE_SERVICE_NAME) {
+		var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+		mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
+		mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
+		mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
+		mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
+		mongoUser = process.env[mongoServiceName + '_USER'];
+
+	// If using env vars from secret from service binding  
+	} else if (process.env.database_name) {
+		mongoDatabase = process.env.database_name;
+		mongoPassword = process.env.password;
+		mongoUser = process.env.username;
+		var mongoUriParts = process.env.uri && process.env.uri.split("//");
+		if (mongoUriParts.length == 2) {
+			mongoUriParts = mongoUriParts[1].split(":");
+			if (mongoUriParts && mongoUriParts.length == 2) {
+				mongoHost = mongoUriParts[0];
+				mongoPort = mongoUriParts[1];
+			}
+		}
+	}
+
+	if (mongoHost && mongoPort && mongoDatabase) {
+		mongoURLLabel = mongoURL = 'mongodb://';
+		if (mongoUser && mongoPassword) {
+			mongoURL += mongoUser + ':' + mongoPassword + '@';
+		}
+		// Provide UI label that excludes user id and pw
+		mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+		mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+	}
+}
+
+function initDb(callback) {
+	if (mongoURL == null) return;
+
+	var mongodb = require('mongodb');
+	if (mongodb == null) return;
+
+	mongodb.connect(mongoURL, function(err, conn) {
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		db = conn;
+		dbDetails.databaseName = db.databaseName;
+		dbDetails.url = mongoURLLabel;
+		dbDetails.type = 'MongoDB';
+
+		console.log('Connected to MongoDB at: %s', mongoURL);
+		console.log('database name: %s', dbDetails.databaseName );
+		
+		rawTransactionsCollection = db.collection('rawtransactions'); 
+		transactionsCollection = db.collection('transactions'); 
+	});
+};
+
+function clearTransactionTables(){
+	rawTransactionsCollection.drop(function(err, delok1){
+		if(err) {
+			console.log(err);
+		};
+		transactionsCollection.drop(function(err,delok2){
+			if (err) {
+				console.log(err);
+			};
+		});
+	});
+};
+
+function getAllRawTransactions(){
+	rawTransactionsCollection.find().toArray(function(err, docs){
+		console.log(docs);
+		
+		return docs;
+	})
+};
+	
+function insertRawTransaction(line){
+	rawTransactionsCollection.insert({type: 'plaintext', text: line});
+};
+	
+function insertParsedTransaction(){
+		return 'hello';	
+};
+
+module.exports = {
+	initDb:initDb,
+	clearTransactionTables:clearTransactionTables,
+	getAllRawTransactions:getAllRawTransactions,
+	insertRawTransaction:insertRawTransaction,
+	insertParsedTransaction:insertParsedTransaction
+};
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan'),
-    fs = require('fs');
-    bodyParser = require('body-parser');
-		multer  = require('multer');
-		upload  = multer({ dest: __dirname+'/tmp/'});
-		readline = require('readline');
+		app     = express(),
+		morgan  = require('morgan'),
+		fs = require('fs'),
+		bodyParser = require('body-parser'),
+		multer  = require('multer'),
+		upload  = multer({ dest: __dirname+'/tmp/'}),
+		readline = require('readline'),
+		dataParsing = require(__dirname+'/app/dataParsingMethods'),
+		dbMethods = require(__dirname+'/app/databaseMethods');
 		
-Object.assign=require('object-assign')
+Object.assign=require('object-assign');
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
@@ -16,139 +18,15 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8090,
-    ip   = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1:27017/chestshop',
-    mongoURLLabel = "";
-
-if (mongoURL == null) {
-  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
-  // If using plane old env vars via service discovery
-  if (process.env.DATABASE_SERVICE_NAME) {
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-    mongoUser = process.env[mongoServiceName + '_USER'];
-
-  // If using env vars from secret from service binding  
-  } else if (process.env.database_name) {
-    mongoDatabase = process.env.database_name;
-    mongoPassword = process.env.password;
-    mongoUser = process.env.username;
-    var mongoUriParts = process.env.uri && process.env.uri.split("//");
-    if (mongoUriParts.length == 2) {
-      mongoUriParts = mongoUriParts[1].split(":");
-      if (mongoUriParts && mongoUriParts.length == 2) {
-        mongoHost = mongoUriParts[0];
-        mongoPort = mongoUriParts[1];
-      }
-    }
-  }
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-  }
-}
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-    console.log('database name: %s', dbDetails.databaseName );
-  });
-};
-
+		ip   = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+		
 app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-app.get('/maketransaction', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('transactions');
-    // Create a document with request IP and current time of request
-    col.insert({type: 'sale', amount:1, date: Date.now()});
-    res.send('transaction logged');
-    
-  } else {
-    res.send('error');
-  }
-});
-
-app.get('/listtransactions', function (req, res) {
-  // list collections
-  if (!db) {
-    initDb(function(err){});
-  }	
-  if (db) {
-  	db.collection('transactions', function(err, collection){
-  		collection.find().toArray(function(err, docs){
-  			console.log(docs);
-  			res.render('transactions.html',{transactionsArray: docs});
-  		})
-  	});
-  } else {
-    res.send('error');
-  }
+	dbMethods.initDb(function(err){
+		res.send('Error connecting to Mongo. Message:\n'+err);
+	});
+	// list collections
+	var transactions = dbMethods.getAllRawTransactions();
+	res.render('transactions.html',{transactionsArray: transactions});
 });
 
 app.get('/getfile', function(req,res){
@@ -156,30 +34,34 @@ app.get('/getfile', function(req,res){
 });
 
 app.post('/getfile', upload.single('rawtransactions'), function(req,res){
-	console.log(req.file);
-  console.log(req.file.path);
-  console.log(req.file.type);
-  var file = __dirname + "/" + req.file.name;
-  var rl = readline.createInterface({
-		input: fs.createReadStream(req.file.path),
-    crlfDelay: Infinity
-  });
-  
-  rl.on('line', (line) => {
-	  console.log(`Line from file: ${line}`);
+	dbMethods.initDb(function(err){
+		res.send('Error connecting to Mongo. Message:\n'+err);
 	});
-  
-  res.end('file read complete');
+	if (req.file != null){
+		dbMethods.clearTransactionTables();
+		var rl = readline.createInterface({
+			input: fs.createReadStream(req.file.path),
+			crlfDelay: Infinity
+		});
+		rl.on('line', (line) => {
+			dbMethods.insertRawTransaction(line);
+		});				
+	}else{
+		res.end('no file selected');	
+	}
+
+	//var hello = dataParsing.parseTransaction();
+	res.redirect('/');
 });
 
 // error handling
 app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+	console.error(err.stack);
+	res.status(500).send('Something bad happened!');
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
+dbMethods.initDb(function(err){
+	console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
 app.listen(port, ip);
