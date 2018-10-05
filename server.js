c71a8976bb363fc60410d96d7e1de1ae52ac7333
Patrick Douglas <pmdouglas@gmail.com>
2018-10-05 11:14:55 -0400

    env variables
    env variabls

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(morgan('combined'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var port = process.env.OPENSHIFT_NODEJS_PORT ||process.env.NODEJS_PORT,
-		ip   = process.env.OPENSHIFT_NODEJS_IP || process.env.NODEJS_IP;
+var port = process.env.NODEJS_PORT||process.env.OPENSHIFT_NODEJS_PORT ||8080,
+		ip   = process.env.NODEJS_IP || process.env.OPENSHIFT_NODEJS_IP||'0.0.0.0';
 		
 app.get('/', function (req, res) {
 	dbMethods.initDb(function(err){
